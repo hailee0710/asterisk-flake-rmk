c33878f674e6e5bab63fce24e59f829635d94542
Hai Lee <haitho0710@gmail.com>
2026-08-04 18:54:33 +0700

    fix: register pointing processor in central for PMW3610 trackball

--- src/central.rs
+++ src/central.rs
@@ -1,7 +1,15 @@
 #![no_main]
 #![no_std]
 
+use rmk::input_device::pointing::PointingProcessorController;
 use rmk::macros::rmk_central;
 
 #[rmk_central]
-mod keyboard_central {}
+mod keyboard_central {
+    use super::*;
+
+    #[register_processor(event)]
+    fn pointing_processor_controller() -> PointingProcessorController {
+        PointingProcessorController::new()
+    }
+}
